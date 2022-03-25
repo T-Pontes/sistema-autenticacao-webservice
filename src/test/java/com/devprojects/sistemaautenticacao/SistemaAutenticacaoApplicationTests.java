b1d360e16f023ba60d912b67a541798665fdcc25
T-Pontes <tiagopontes10@gmail.com>
2022-03-24 21:32:55 -0300

    Projeto criado

---
+++ src/test/java/com/devprojects/sistemaautenticacao/SistemaAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprojects.sistemaautenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
