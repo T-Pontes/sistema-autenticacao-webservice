cba31987ec5308ea36a8eb67426885c53485a0ea
T-Pontes <tiagopontes10@gmail.com>
2022-03-28 20:56:12 -0300

    Banco de dados H2 e criacao automatica da base de dados com jpa

--- src/main/java/com/devprojects/sistemaautenticacao/entities/Perfil.java
+++ src/main/java/com/devprojects/sistemaautenticacao/entities/Perfil.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -11,6 +13,7 @@ public class Perfil implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String descricao;
 	
